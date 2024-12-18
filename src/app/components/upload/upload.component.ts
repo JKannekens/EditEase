8238abeee4634dc3358cab8fe156c1a93ca03a83
Justin Kannekens <justinkannekens@gmail.com>
2024-12-18 21:29:01 +0100

    Add pdf viewer

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-upload',
-  imports: [],
+  imports: [PdfViewerModule],
   templateUrl: './upload.component.html',
 })
 export class UploadComponent {
   selectedFile: File | null = null;
+  selectedFileUrl: string | null = null;
 
-  onButtonClick(): void {
+  onFileUploadClick(): void {
     const inputElement = document.getElementById('fileInput');
     if (!inputElement) return;
     inputElement.click();
   }
 
-  onFileSelected(event: any): void {
-    const file = event.target.files[0];
-    if (!file) return;
+  onFileRemoveClick(): void {
+    this.selectedFile = null;
+    this.selectedFileUrl = null;
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.item(0);
+
+    if (!file || !file.type.includes('pdf')) return;
     this.selectedFile = file;
+    this.selectedFileUrl = URL.createObjectURL(file);
   }
 }
