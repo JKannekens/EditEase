cd9b421c0e1ace65e3789385071b42b8a5da82dc
Justin Kannekens <justinkannekens@gmail.com>
2024-12-18 20:41:27 +0100

    Add file upload

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './upload.component.html',
 })
-export class UploadComponent {}
+export class UploadComponent {
+  selectedFile: File | null = null;
+
+  onButtonClick(): void {
+    const inputElement = document.getElementById('fileInput');
+    if (!inputElement) return;
+    inputElement.click();
+  }
+
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (!file) return;
+    this.selectedFile = file;
+  }
+}
